07d9d04c6a76f6b4ed86ce976d3c78a114fd00df
Jeremy <ironlikehead@gmail.com>
2023-04-11 21:14:13 +0800

    unverify perms changed

--- events/commands.js
+++ events/commands.js
@@ -1,11 +1,29 @@
 const memberRole = "288385193285386248"
 const guestRole = "615837413117526027"
+const grandcounsilRole = "288382736480337920"
 
 module.exports = {
     name: "interactionCreate",
     async execute(interaction) {
         
         if (interaction.commandName === "unverify") {
+
+            const interactor = await interaction.guild.members.fetch(interaction.user.id)
+
+            if (!interactor.roles.cache.has(grandcounsilRole)) {
+                interaction.reply({ content: "You are not authorised to perform this action", ephemeral: true})
+
+                setTimeout(async () => {
+                    try {
+                        await interaction.deleteReply()
+                    } catch (error) {
+                        console.error(error)
+                    }
+                },5000)
+
+                return;
+            }
+
             const member = await interaction.options.get("discord-user")
             const userMember = await interaction.guild.members.fetch(member.user.id)
             await userMember.roles.remove(memberRole)
