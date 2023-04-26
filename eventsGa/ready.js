5603c1a03b83cea70c958b83fe5acbc31ea5af43
Jeremy <ironlikehead@gmail.com>
2023-04-27 01:17:13 +0800

    new application added

---
+++ eventsGa/ready.js
@@ -0,0 +1,9 @@
+const { Events, ActivityType } = require("discord.js")
+
+module.exports = {
+    name: "ready",
+    once: true,
+    async execute(client) {
+        console.log(`${client.user.tag} is live`);
+    }
+}
